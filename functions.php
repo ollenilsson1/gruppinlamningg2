cc0588ba57c9519bfeec775955b7fd186edf1309
Nant1ng <andressantana99@hotmail.se>
2021-09-21 13:57:44 +0200

    Enqueued script for bootstrap

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function wpbootstrap_enqueue_styles()
 {
     wp_enqueue_style('bootstrap', '//cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css');
     wp_enqueue_style('my-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_script('bootstrap',  'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js', array(), 1, 1, 1);
 }
 
 add_action('wp_enqueue_scripts', 'wpbootstrap_enqueue_styles');
