436d0db5d6cf732ca662cb7db94e0e7a154ea0cc
Nant1ng <andressantana99@hotmail.se>
2021-09-21 11:10:19 +0200

    Woocommerce

--- functions.php
+++ functions.php
@@ -27,6 +27,11 @@ register_nav_menus(
     )
 );
 
+// Woocommerce
+function mytheme_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
 
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
 add_theme_support('post-thumbnails');
