bedb1efc34ce97bce64373c6dd7992bf97e00ca8
Nant1ng <andressantana99@hotmail.se>
2021-09-24 17:44:08 +0200

    FMT

--- contact.php
+++ contact.php
@@ -1,25 +1,25 @@
-<?php 
+<?php
 /*
 Template Name: kontakt
 */
 ?>
-<?php 
+<?php
 get_header();
 ?>
 
 <main>
-<section>
-    <div class="container">
-        <div class="row">
-            <div id="primary" class="col-xs-12 col-md-9">
-                <h1><?php the_title(); ?></h1>
-                <p><?php the_content(); ?></p>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div id="primary" class="col-xs-12 col-md-9">
+                    <h1><?php the_title(); ?></h1>
+                    <p><?php the_content(); ?></p>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 </main>
 
-<?php 
+<?php
 get_footer();
 ?>
