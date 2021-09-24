862f202077ea77174e334796dce9e1c220638e6b
Nant1ng <andressantana99@hotmail.se>
2021-09-24 18:01:49 +0200

    FMT

--- page.php
+++ page.php
@@ -1,10 +1,7 @@
 <?php get_header(); ?>
 
-
-
 <div class="content">
     <div class="container">
-
         <div class="row">
             <div class="col-lg-3">
                 <div class="sticky-top">
@@ -12,20 +9,18 @@
                 </div>
             </div>
             <div class="col-lg-9">
-                    <?php if(has_post_thumbnail()) : ?>
-                        <img src="<?php the_post_thumbnail_url('post_image'); ?>" alt="<?php the_title(); ?>" class="img-fluid mb-5"/>
-                    <?php endif; ?>
+                <?php if (has_post_thumbnail()) : ?>
+                    <img src="<?php the_post_thumbnail_url('post_image'); ?>" alt="<?php the_title(); ?>" class="img-fluid mb-5" />
+                <?php endif; ?>
                 <h1>
-                    <?php the_title()?> 
+                    <?php the_title() ?>
                 </h1>
-                <?php if(have_posts()) : while(have_posts()) : the_post()?>
-                <?php the_content();?>
-                <?php endwhile; else: endif; ?>
+                <?php if (have_posts()) : while (have_posts()) : the_post() ?>
+                        <?php the_content(); ?>
+                <?php endwhile;
+                else : endif; ?>
             </div>
         </div>
-        
-       
-       
     </div>
 </div>
 
