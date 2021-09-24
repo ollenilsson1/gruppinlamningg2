08d308047434b124e191f9dd805babd6bfb71452
Nant1ng <andressantana99@hotmail.se>
2021-09-24 17:44:34 +0200

    FMT

--- front-page.php
+++ front-page.php
@@ -5,25 +5,25 @@ $data = get_field('slides', 33);  //OLLE 33, ANDRES 48, MALIN 77, CORNELIA 69
 ?>
 
 <main>
-<section>
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2" class="active"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="<?php echo $data['image_1']['url']; ?>" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="<?php echo $data['image_2']['url']; ?>" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="<?php echo $data['image_3']['url']; ?>" alt="Third slide">
-    </div>
-</section>
-  
+  <section>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2" class="active"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="<?php echo $data['image_1']['url']; ?>" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="<?php echo $data['image_2']['url']; ?>" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="<?php echo $data['image_3']['url']; ?>" alt="Third slide">
+        </div>
+  </section>
+
   <section>
 
 
