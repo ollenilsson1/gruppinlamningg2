bbbbb7fceb8474e14bba65038e4955bd474ddbb3
[Olle Nilsson] <[olle.nilsson@medieinstitutet.se]>
2021-10-05 14:41:59 +0200

    404 sida

---
+++ 404.php
@@ -0,0 +1,32 @@
+
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+
+
+<header class="page-header">
+                <h1 class="page-title"><?php _e( 'Not Found', 'sportstore' ); ?></h1>
+            </header>
+ 
+            <div class="page-wrapper">
+                <div class="page-content">
+                    <h2><?php _e( 'Något blev fel', 'sportstore' ); ?></h2>
+                    <p><?php _e( 'Vi kunde inte hitta sidan, prova sök eller använd menyn', 'sportstore' ); ?></p>
+ 
+</div>
+
+
+
+
+
+</div>
+
+
+
+<?php get_footer(); ?>
